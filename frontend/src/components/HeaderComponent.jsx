85e5d71ac362c24d75a0592cb195aa7a948b36a0
Ngoc Thinh <thinhdnde150052@fpt.edu.com>
2023-12-29 12:39:30 +0700

    Update UI

--- frontend/src/components/HeaderComponent.jsx
+++ frontend/src/components/HeaderComponent.jsx
@@ -3,8 +3,12 @@ import styled from "@emotion/styled";
 import Link from "next/link";
 import Image from "next/image";
 import logo from "../../public/logo.png";
-import { Avatar, Layout, Menu, Space, Button } from "antd";
+import { Avatar, Layout, Menu, Space, Button, Dropdown } from "antd";
 import { useUserState } from "@/recoils/user.state.js";
+import { useDriverState } from "@/recoils/driver.state";
+import { LogoutOutlined, UserOutlined } from "@ant-design/icons";
+import useLocalStorage from "@/hooks/useLocalStorage";
+import { useRouter } from "next/router";
 const StyledMenu = styled(Menu)`
   li {
     &::after {
@@ -19,7 +23,39 @@ const StyledMenu = styled(Menu)`
 const { Header } = Layout;
 export default function HeaderComponent() {
   const [user, setUser] = useUserState();
-
+  const [driver, setDriver] = useDriverState();
+  const router = useRouter();
+  const { pathname, push } = useRouter();
+  const [accessToken, setAccessToken, clearAccessToken] =
+    useLocalStorage("access_token");
+  const items = [
+    {
+      label: (
+        <div onClick={() => push("/profile")}>
+          <UserOutlined className="mr-2" />
+          Thông tin cá nhân
+        </div>
+      ),
+      key: "0",
+    },
+    {
+      label: (
+        <div
+          onClick={() => {
+            clearAccessToken();
+            setUser(null);
+            setDriver(null);
+            router.push("/");
+          }}
+        >
+          {" "}
+          <LogoutOutlined className=" text-red-600 mr-2" />
+          Đăng xuất
+        </div>
+      ),
+      key: "1",
+    },
+  ];
   return (
     <Header
       style={{
@@ -73,18 +109,25 @@ export default function HeaderComponent() {
             </Link>
           </Space>
         ) : (
-          <Link href="/profile">
-            <div className="flex items-center gap-2 shrink-0">
-              <div className="flex bg-neutral-200 rounded-full p-1 cursor-pointer">
+          <div className="flex items-center gap-2 shrink-0">
+            <div className="flex rounded-full p-1 cursor-pointer">
+              <Dropdown
+                className="cursor-pointer"
+                menu={{
+                  items,
+                }}
+                placement="bottom"
+                trigger={["click"]}
+              >
                 {user?.result?.profilePicture ? (
                   <Avatar src={user?.result?.profilePicture} />
                 ) : (
                   <UserFilledIcon className="text-neutral-500" />
                 )}
-              </div>
-              <span>{user?.result?.fullname}</span>
+              </Dropdown>
             </div>
-          </Link>
+            <span>{user?.result?.fullname}</span>
+          </div>
         )}
       </div>
     </Header>
