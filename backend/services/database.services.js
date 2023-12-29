3d93a732a2f30ebcbbd3064a972fd10a8f1dc33c
Ngoc Thinh <thinhdnde150052@fpt.edu.com>
2023-12-29 12:39:57 +0700

    fix code

--- backend/services/database.services.js
+++ backend/services/database.services.js
@@ -2,13 +2,14 @@ import { MongoClient } from 'mongodb'
 import { config } from 'dotenv'
 import mongoose from 'mongoose'
 config()
-const uri = 'mongodb+srv://anhsangprovl:anhsangprovl@cluster0.defhizq.mongodb.net/?retryWrites=true&w=majority'
+const uri =
+  'mongodb+srv://messithinh12345:iz6OH5hpuU1f2jmV@cluster0.jnlhnou.mongodb.net/personal-project?retryWrites=true&w=majority'
 
 class databaseService {
   connect() {
     return mongoose
       .connect(
-        'mongodb+srv://anhsangprovl:anhsangprovl@cluster0.defhizq.mongodb.net/rental-car?retryWrites=true&w=majority'
+        'mongodb+srv://messithinh12345:iz6OH5hpuU1f2jmV@cluster0.jnlhnou.mongodb.net/personal-project?retryWrites=true&w=majority'
       )
       .then(() => {
         console.log('Connected to MongoDB')
